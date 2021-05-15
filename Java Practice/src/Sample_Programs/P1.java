d3327816e489c60b4bfd6586e958cbb0025398f0
Ajay <ajayb015@gmail.com>
2021-05-15 13:27:09 +0530

    Sample_Program package is added

---
+++ Java Practice/src/Sample_Programs/P1.java
@@ -0,0 +1,14 @@
+package Sample_Programs;
+
+class P1 
+{
+	public static void main(String[] args) 
+	{
+		int a=10;  //10
+		int b=a--; //10
+		int c=a++;  //9
+		System.out.println(a);  //10
+		System.out.println(b);  //10
+		System.out.println(c);  //9
+	}
+}
